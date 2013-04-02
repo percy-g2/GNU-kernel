e49db018fb5fff979776dfddf3e3ede5b2210465
percy <gahlotercy@gmail.com>
2013-04-02 15:19:14 +0530

    GNU-common-include

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sat Mar 30 16:28:13 IST 2013"
+#define LINUX_COMPILE_TIME "4:26PM IST 29/3/13 +%T"
+#define LINUX_COMPILE_BY "percy"
+#define LINUX_COMPILE_HOST "GNU"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
